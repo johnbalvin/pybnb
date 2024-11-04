441f6c1531ddbe7444369c23fe2f3511906cd1dd
John <johnchristian@hotmail.es>
2024-11-04 15:54:49 -0500

    new updates

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = '0.0.5'
+VERSION = '0.0.12'
 DESCRIPTION = 'Airbnb scraper in Python'
 
 setup(
@@ -10,7 +10,7 @@ setup(
     author_email="<johnchristian@hotmail.es>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    url='https://github.com/johnbalvin/pybnb',
+    url='https://github.com/johnbalvin/pyairbnbb',
     long_description=open('README.md').read(),
     keywords=['airbnb', 'scraper', 'crawler'],
     install_requires=['curl_cffi','bs4'],
